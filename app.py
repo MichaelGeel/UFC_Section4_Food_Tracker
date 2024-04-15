0c4deafd5469a0dca7490baa8fcca3051bc2668a
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-04-15 20:06:54 +0200

    Lesson 30 Completed
    This lesson covered initializing the routes for the app as well as rendering the html files for the routes and implementing the styling for the pages.

--- app.py
+++ app.py
@@ -1,2 +1,22 @@
 from flask import Flask, url_for, redirect, g, session, render_template, request, jsonify
 import sqlite3
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/food')
+def food():
+    return render_template('add_food.html')
+
+@app.route('/view')
+def view():
+    return render_template('day.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
