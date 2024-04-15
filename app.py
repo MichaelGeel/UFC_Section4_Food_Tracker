a5800bf3debf357a495ef74a975890cac3491845
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-04-15 20:59:57 +0200

    Lesson 32 Completed
    In this lesson we implemented the databse helper functions for getting the database connection that allows interacting with the database as well as writing the teardown for auto-closing the connection after a route has completed its execution.

--- app.py
+++ app.py
@@ -5,6 +5,21 @@ import sqlite3
 app = Flask(__name__)
 
 
+def connect_db():
+    sql = sqlite3.connect('D:\Learning\The Ultimate Flask Course\Section4_Food_Tracker\food_log.db')
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db():
+    if not hasattr(g, 'sqlite3_db'):
+        g['sqlite_db'] = connect_db()
+    return g['sqlite_db']
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g['sqlite_db'].close()
+
 @app.route('/')
 def index():
     return render_template('home.html')
