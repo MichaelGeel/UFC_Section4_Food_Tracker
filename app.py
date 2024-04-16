70f2cd0511cf32bd468d42805ad6111b8bb80ec5
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-04-16 21:51:17 +0200

    Lesson 33 Completed
    Developed the form submit functionalities that allows us to submit form data for adding a food item to the database to the POST endpoint of the food route.

--- app.py
+++ app.py
@@ -6,27 +6,39 @@ app = Flask(__name__)
 
 
 def connect_db():
-    sql = sqlite3.connect('D:\Learning\The Ultimate Flask Course\Section4_Food_Tracker\food_log.db')
+    sql = sqlite3.connect('D:\Learning\The Ultimate Flask Course\Section4_Food_Tracker/food_log.db')
     sql.row_factory = sqlite3.Row
     return sql
 
 def get_db():
     if not hasattr(g, 'sqlite3_db'):
-        g['sqlite_db'] = connect_db()
-    return g['sqlite_db']
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
 
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'sqlite_db'):
-        g['sqlite_db'].close()
+        g.sqlite_db.close()
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
-@app.route('/food')
+@app.route('/food', methods=['GET', 'POST'])
 def food():
-    return render_template('add_food.html')
+    if request.method == "POST":
+
+        name = request.form['food-name']
+        carbs = request.form['carbohydrates']
+        protein = request.form['protein']
+        fat = request.form['fat']
+        calories = (protein*4) + (carbs*4) + (fat*9)
+
+        db = get_db()
+        
+        return "<h1>{}{}{}{}</h1>".format(request.form['food-name'], request.form['protein'], request.form['carbohydrates'], request.form['fat'])
+    else:
+        return render_template('add_food.html')
 
 @app.route('/view')
 def view():
